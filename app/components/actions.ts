369064bc00cc3792cb4c1e37c15b8924ad067368
Tobias Belfiore <tbelfiore@beskar.net>
2025-02-26 15:30:37 -0300

    a

---
+++ app/components/actions.ts
@@ -0,0 +1,53 @@
+"use server"
+import { cookies } from "next/headers"
+
+export const getCookie = async (cookieName: string) => {
+
+  try {
+    const cookieStore = await cookies();
+    const cookie = await cookieStore.get(cookieName)
+    return cookie?.value;
+  } catch (error) {
+    return 'error'
+  }
+
+}
+
+export const setCookie = async (name: string, value: string) => {
+
+  try {
+    const cookieStore = await cookies();
+    await cookieStore.set(name, value)
+  } catch (error) {
+    return null
+  }
+
+}
+
+export const setCookieE = async (name: string, value: string) => {
+
+  try {
+    const cookieStore = await cookies();
+    await cookieStore.set(name, value, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge : 3600
+    }
+    )
+  } catch (error) {
+    return null
+  }
+
+}
+
+export const deleteCookie = async (cookieName: string) => {
+
+  try {
+    const cookieStore = await cookies();
+    await cookieStore.delete(cookieName);
+    return 'eliminada'
+  } catch (error) {
+    return 'error'
+  }
+
+}
